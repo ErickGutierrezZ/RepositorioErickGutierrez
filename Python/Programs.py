c695f0a72c5a58e4483b81bc571c34c3fa11cac4
ErickGutierrezZ <111941632+ErickGutierrezZ@users.noreply.github.com>
2022-08-24 21:59:25 -0500

    Add files via upload

---
+++ Python/Programs.py
@@ -0,0 +1,11 @@
+print ('Hola mundo')
+a = int(input('Escribe un número'))
+b = float(input('Escribe otro número'))
+print ("El resultado es:"+ str(a))
+print ("El otro número es:" , (b))
+
+#+         -        *                  //                /                **       (1/2)     y      o
+#Suma    Resta   Multiplicacion     Division con y sin decimal       Potencia       Raiz
+
+
+#Hacer un programa que lea cuatro números y muestre el promedio de ellos
---
+++ Python/print ('Promedio cuatro numeros').py
@@ -0,0 +1,10 @@
+print ('Promedio cuatro numeros')
+a = float(input('Escribe un número'))
+b = float(input('Escribe otro número'))
+c = float(input('Escribe otro número'))
+d = float(input('Escribe otro número'))
+
+sum = a+b+c+d
+prome = sum/4
+
+print ("El resultado es:"+ str(prome)) 
